fdf77da889150c1ac78fe5e0563b0b79a71085b1
Md Shah Alam Shamim <shamimewu.orbund@gmail.com>
2020-04-13 08:59:10 +0600

    simple sample project is done

--- src/main/java/com/orbund/galactic/be/common/sample/GalacticBeCommonSampleApplication.java
+++ src/main/java/com/orbund/galactic/be/common/sample/GalacticBeCommonSampleApplication.java
@@ -1,7 +1,14 @@
 package com.orbund.galactic.be.common.sample;
 
 import com.orbund.galactic.be.common.entities.GalacticBeCommonEntitiesApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-public class GalacticBeCommonSampleApplication extends GalacticBeCommonEntitiesApplication {
-
+@SpringBootApplication
+@Import(GalacticBeCommonEntitiesApplication.class)
+public class GalacticBeCommonSampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GalacticBeCommonSampleApplication.class, args);
+    }
 }
